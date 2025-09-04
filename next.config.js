64bbe1c46d7dc177c579a36842fce68337a95c95
Marawan Mokhtar <105330382+marawan10@users.noreply.github.com>
2025-09-04 13:22:02 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  basePath: '/my-portfolio',
-  assetPrefix: '/my-portfolio',
-  images: {
-    unoptimized: true
-  }
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
